dcf065149ce49c6da861623ca9c17f6a0b0c4d42
Zhansaule Telisheva <jansaule97@mail.ru>
2023-08-28 19:09:10 +0500

    feat: update HTML and CSS

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-// create Elements in the body
-const container = document.createElement('div');
-const title = document.createElement('h1');
-const textarea = document.createElement('textarea');
-const keyboardContainer = document.createElement('div');
+// // create Elements in the body
+// const container = document.createElement('div');
+// const title = document.createElement('h1');
+// const textarea = document.createElement('textarea');
+// const keyboardContainer = document.createElement('div');
+// const keyboardRow = document.createElement('div');
 
-// append all of the elements
-document.body.appendChild(container);
-container.appendChild(title);
-container.appendChild(textarea);
-container.appendChild(keyboardContainer);
+// // append all of the elements
+// document.body.appendChild(container);
+// container.appendChild(title);
+// container.appendChild(textarea);
+// container.appendChild(keyboardContainer);
 
-// add class names
-container.classList.add('container');
-title.classList.add('title');
-textarea.classList.add('textarea');
-keyboardContainer.classList.add('keyboard');
+// for (let i = 0; i < 5; i++) {
+//   keyboardContainer.appendChild(keyboardRow);
+// }
 
-// add the title 
-title.innerHTML = "RSS Виртуальная клавиатура";
+
+// // add class names
+// container.classList.add('container');
+// title.classList.add('title');
+// textarea.classList.add('textarea');
+// keyboardContainer.classList.add('keyboard');
+// keyboardRow.classList.add('row');
+
+// // add the title 
+// title.innerHTML = "RSS Виртуальная клавиатура";
 
 
 
